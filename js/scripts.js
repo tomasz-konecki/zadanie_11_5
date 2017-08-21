f9a8385aa5766f9afdacee0047bf33bd9f238502
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-08-21 20:00:10 +0200

    Finish task

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(function() {
 
+    function Button(text) {
+        this.text = text || 'Hello';
+    }
+    Button.prototype = {
+        create: function() {
+            var self = this;
+            this.$element = $('<button>');
+            this.$element.text(this.text);
+            this.$element.click(function() {
+                alert(self.text);
+            });
+            $('body').append(this.$element);
+        }
+    }
+
+    var btn1 = new Button('Welcome to my webpage!');
+
+    btn1.create();
+
 });
